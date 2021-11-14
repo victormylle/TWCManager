5297527ad78deefde775b4a7c35c60a1b3160836
Victor Mylle <victor.mylle@hotmail.com>
2021-11-14 12:59:17 +0100

    print debug

--- lib/TWCManager/TWCSlave.py
+++ lib/TWCManager/TWCSlave.py
@@ -443,7 +443,7 @@ class TWCSlave:
             # TODO: Start and stop charging using protocol 2 commands to TWC
             # instead of car api if I ever figure out how.
         
-            Logger.debug("I want to stop charging")
+            print("I want to stop charging")
 
             if self.lastAmpsOffered == 0 and self.reportedAmpsActual > 4.0:
                 now = time.time()
